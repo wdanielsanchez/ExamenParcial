bef89a14eeb7a882e7b182397ed8699ec685081b
WalterSanchezIng <waltersanchez.ing@gmail.com>
2024-11-13 18:26:27 -0500

    Initial commit

---
+++ ExamenParcial/src/main/java/com/ExamenParcialSanchez/ExamenParcial/ExamenParcialApplication.java
@@ -0,0 +1,13 @@
+package com.ExamenParcialSanchez.ExamenParcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenParcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenParcialApplication.class, args);
+	}
+
+}
---
+++ ExamenParcial/src/test/java/com/ExamenParcialSanchez/ExamenParcial/ExamenParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ExamenParcialSanchez.ExamenParcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
